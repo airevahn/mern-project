ee26b5686b30a669620875bdd18026980d063545
airevahn <ivan.suryawinata@gmail.com>
2022-04-08 01:39:57 +0800

    complete commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 // app.js
 
 const express = require('express');
+const connectDB = require('./config/db'); // updated after DB created
+var cors = require('cors');
 
+// Routes
+const books = require('./routes/api/books');
 const app = express();
 
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
